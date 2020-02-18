e047ccd027969fc132501d67ccc619e1de5dcba4
Ymt <tuanit2304@gmail.com>
2020-02-18 09:35:24 +0700

    enum class, how about abstract class ?

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,34 @@
 package Main
 
-class Fruit {
-    private var watermelon: String = "wtm"
-    private var grapes: String = "Dautay"
-    private var greenApple: String = "GA"
-    class Nested {
-        fun sayHello(){
-            println("a function inside a NESTED class")
-        }
-    }
-    inner class Inner {
-        fun getApple():String {
-            println("a function inside an INNER class")
-            println("apple is $greenApple")
-            return "1"
-        }
-    }
+enum class CompassDirection{
+    EAST,
+    WEST,
+    SOUTH,
+    NORTH
 }
-
+enum class Color(val red: Int, val green:Int, val blue:Int){
+    RED(255,0,0),
+    GREEN(0, 255, 0),
+    BLUE(0,0,255)
+}
+enum class ActionState {
+    ACTIVE {
+        override fun reverseAction() = INACTIVE
+    },
+    INACTIVE {
+        override fun reverseAction() = ACTIVE
+    };
+    abstract fun reverseAction(): ActionState
+}
 fun main(args: Array<String>){
-    Fruit.Nested().sayHello()
-    var fruit = Fruit()
-    var innerFruit = fruit.Inner()
-    innerFruit.getApple()
+    println("North enum = ${CompassDirection.NORTH}")
+    println(Color.BLUE.toString())
+    println("Color Details.Name = ${Color.GREEN.name}, ordinal = ${Color.GREEN.ordinal}")
+    println(Color.valueOf("BLUE"))
+    //println(Color.valueOf("BLUE123"))
+    for(colorValue in Color.values()) {
+        println("color value is : $colorValue")
+    }
+    println(ActionState.ACTIVE.reverseAction())
+    println(ActionState.INACTIVE.reverseAction())
 }
