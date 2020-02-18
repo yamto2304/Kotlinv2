67f007027607b32d0d90a5dc1d66d7d9b98324ed
Ymt <tuanit2304@gmail.com>
2020-02-18 09:53:14 +0700

    delegation - what is interface ? diff from class ?

--- src/Main.kt
+++ src/Main.kt
@@ -1,34 +1,22 @@
 package Main
 
-enum class CompassDirection{
-    EAST,
-    WEST,
-    SOUTH,
-    NORTH
+interface BirdInterface {
+    fun flyAndFindFood()
 }
-enum class Color(val red: Int, val green:Int, val blue:Int){
-    RED(255,0,0),
-    GREEN(0, 255, 0),
-    BLUE(0,0,255)
+
+class Eagle(val age: Int) : BirdInterface {
+    override fun flyAndFindFood() {
+        println("I'm an eagle, I'm $age  years old.")
+    }
 }
-enum class ActionState {
-    ACTIVE {
-        override fun reverseAction() = INACTIVE
-    },
-    INACTIVE {
-        override fun reverseAction() = ACTIVE
-    };
-    abstract fun reverseAction(): ActionState
+class Cuckoo(b : BirdInterface):BirdInterface by b {
+    //    override fun flyAndFindFood() {
+    //        println("I'm a cuckoo. I'm flying and finding food")
+    //    }
 }
 fun main(args: Array<String>){
-    println("North enum = ${CompassDirection.NORTH}")
-    println(Color.BLUE.toString())
-    println("Color Details.Name = ${Color.GREEN.name}, ordinal = ${Color.GREEN.ordinal}")
-    println(Color.valueOf("BLUE"))
-    //println(Color.valueOf("BLUE123"))
-    for(colorValue in Color.values()) {
-        println("color value is : $colorValue")
-    }
-    println(ActionState.ACTIVE.reverseAction())
-    println(ActionState.INACTIVE.reverseAction())
+    val eagle = Eagle(2)
+    eagle.flyAndFindFood()
+    val cuckoo = Cuckoo(eagle)//interface?
+    cuckoo.flyAndFindFood()
 }
